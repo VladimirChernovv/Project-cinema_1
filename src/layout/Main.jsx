adc70c72d5b823248bb0058f6d0c2cf0d73d09b1
Vladimir Chernov <v.chernov@zebrains.team>
2021-04-13 13:30:51 +0300

    Сделал перерисовку набора фильмов которые уже есть в списке

---
+++ src/layout/Main.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Movies} from '../components/Movies';
+import {Search} from '../components/Search';
+import {Preloader} from '../components/Preloader';
+
+class Main extends React.Component {
+  state = {
+    movies: [],
+  }
+
+  // Метод жизненого цикла
+  componentDidMount() {
+    fetch('http://www.omdbapi.com/?apikey=27cf887b&s=matrix')
+      .then(response => response.json())
+      .then(data => this.setState({movies: data.Search}))
+  }
+
+  searchMovies = (str) => {
+    fetch(`http://www.omdbapi.com/?apikey=27cf887b&s=${str}`)
+      .then(response => response.json())
+      .then(data => this.setState({movies: data.Search}))
+  }
+
+  render() {
+    const {movies} = this.state;
+
+    return <main className="container content">
+      <Search searchMovies={this.searchMovies} />
+      {
+        movies.length ? (
+          <Movies movies={this.state.movies}/>
+        ) : <Preloader />
+      }
+    </main>
+  }
+};
+
+export {Main};
